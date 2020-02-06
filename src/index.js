9fe143c6053f057c00817f1a98182f29fdc0c9bd
Tiago Dela Rosa <tiago.delarosa@live.com>
2020-02-06 18:00:22 -0300

    adicionando variaveis de ambiente

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
+dotenv.config();
 
 // Connect to DB
 mongoose.connect(
-  "mongodb+srv://tiago_lopes:tiago123456@cluster0-7s16h.mongodb.net/test",
-  () => console.log('conectado!')
+  process.env.DB_CONNECT,
+  { useNewUrlParser : true },
+  () => {
+    return console.log('conectado!');
+  }
 );
 
 const app = express()
