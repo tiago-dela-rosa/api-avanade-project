dc31ee4d56ab8c1e95d94d8d5f9103f62f511345
Thiago P <thiagopmaceda@gmail.com>
2020-02-10 01:03:33 -0300

    logica de login e cadastro passada para o controller

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,24 +1,95 @@
-//const UserModel = require("../models/UserSchema");
+var validator = require("validator");
+const { cpf } = require("cpf-cnpj-validator");
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+
+// Passport config
+require("../config/passport")(passport);
 
 module.exports = {
+  register(req, res) {
+    const { password, fullName, email } = req.body;
 
-  store(req, res) {
+    const cpfReq = req.body.cpf;
 
-    const { cpf, email, balance, account_number } = req.body;
+    const balance = 0.0;
+    const numberAccount = "945801-3";
 
-    // Se ja existir usuario com esse cpf
-    const userExist = await UserModel.findOne({ cpf: cpf })
-    if(userExist) {
-      return userExist;
+    // Check para verificar se todos os campos foram preenchidos
+    if (!cpfReq || !password || !fullName || !email) {
+      res.status(400).send({ msg: "Por favor preencha todos os campos." });
+      return false;
     }
 
-    const userRegistred = UserModel.Create({
-      cpf,
-      email,
-      balance,
-      account_number,
-    });
+    //Check de CPF
+    if (!cpf.isValid(cpfReq)) {
+      res.status(400).send({ msg: "Por favor preencha um CPF válido." });
+    }
 
-    return res.send(userRegistred);
+    // Check do email
+    if (!validator.isEmail(email)) {
+      res.status(400).send({ msg: "Preencha um email válido." });
+    }
+
+    // Verificar se usuario existe no banco
+    User.findOne({ cpf: cpfReq })
+      .then(user => {
+        if (user) {
+          res.status(400).send({ msg: "Usuário já cadastrado com esse CPF" });
+        } else {
+          const newUser = new User({
+            cpf: cpfReq,
+            password,
+            email,
+            fullName,
+            balance,
+            numberAccount
+          });
+
+          //Hash na senha
+          bcrypt.genSalt(10, (err, salt) =>
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if (err) throw err;
+
+              //Passar a senha do usuario para hash
+              newUser.password = hash;
+
+              //Persisitir dados do novo usuario no banco
+              newUser
+                .save()
+                .then(user => {
+                  res
+                    .status(200)
+                    .send({ msg: "Usuário cadastrado com sucesso." });
+                })
+                .catch(err => console.log(err));
+            })
+          );
+        }
+      })
+      .catch(error => console.log(error));
+  },
+
+  login(req, res, next) {
+    passport.authenticate("local", async (err, user, info) => {
+      try {
+        if (err || !user) {
+          const error = new Error("An Error occurred");
+          return next(error);
+        }
+        req.login(user, { session: false }, async error => {
+          if (error) return next(error);
+          //salva id e email no token
+          const body = { _id: user._id, email: user.email };
+          const token = jwt.sign({ user: body }, "top_secret");
+          //Manda o token, id e email de volta na resposta
+          return res.json({ token, body });
+        });
+      } catch (error) {
+        return next(error);
+      }
+    })(req, res, next);
   }
-}
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,104 +1,15 @@
 const express = require("express");
-//const UserController = require('./controllers/UserController');
+const UserController = require("../controllers/UserController");
 const routes = express.Router();
-var validator = require("validator");
-const { cpf } = require("cpf-cnpj-validator");
-const User = require("../database/models/User");
-const bcrypt = require("bcrypt");
 const passport = require("passport");
-const jwt = require("jsonwebtoken");
 
 // Passport config
 require("../config/passport")(passport);
 
 // Login
-routes.post("/api/login", passport.authenticate("local"), function(
-  req,
-  res,
-  next
-) {
-  passport.authenticate("local", async (err, user, info) => {
-    try {
-      if (err || !user) {
-        const error = new Error("An Error occurred");
-        return next(error);
-      }
-      req.login(user, { session: false }, async error => {
-        if (error) return next(error);
-        //salva id e email no token
-        const body = { _id: user._id, email: user.email };
-        const token = jwt.sign({ user: body }, "top_secret");
-        //Manda o token, id e email de volta na resposta
-        return res.json({ token, body });
-      });
-    } catch (error) {
-      return next(error);
-    }
-  })(req, res, next);
-});
+routes.post("/api/login", passport.authenticate("local"), UserController.login);
 
 // Cadastrar
-routes.post("/api/register", (req, res) => {
-  const { password, fullName, email } = req.body;
-
-  const cpfReq = req.body.cpf;
-
-  const balance = 0.0;
-  const accountNumber = "945801-1";
-
-  // Check para verificar se todos os campos foram preenchidos
-  if (!cpfReq || !password || !fullName || !email) {
-    res.status(400).send({ msg: "Por favor preencha todos os campos." });
-    return false;
-  }
-
-  //Check de CPF
-  if (!cpf.isValid(cpfReq)) {
-    res.status(400).send({ msg: "Por favor preencha um CPF válido." });
-  }
-
-  // Check do email
-  if (!validator.isEmail(email)) {
-    res.status(400).send({ msg: "Preencha um email válido." });
-  }
-
-  // Verificar se usuario existe no banco
-  User.findOne({ cpf: cpfReq })
-    .then(user => {
-      if (user) {
-        res.status(400).send({ msg: "Usuário já cadastrado com esse CPF" });
-      } else {
-        const newUser = new User({
-          cpf: cpfReq,
-          password,
-          email,
-          fullName,
-          balance,
-          accountNumber
-        });
-
-        //Hash na senha
-        bcrypt.genSalt(10, (err, salt) =>
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-
-            //Passar a senha do usuario para hash
-            newUser.password = hash;
-
-            //Persisitir dados do novo usuario no banco
-            newUser
-              .save()
-              .then(user => {
-                res
-                  .status(200)
-                  .send({ msg: "Usuário cadastrado com sucesso." });
-              })
-              .catch(err => console.log(err));
-          })
-        );
-      }
-    })
-    .catch(error => console.log(error));
-});
+routes.post("/api/register", UserController.register);
 
 module.exports = routes;
