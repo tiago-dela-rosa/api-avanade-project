95aadc3e17788e3084d34f194eb1d37716d0ff0b
Tiago Dela Rosa Lopes <delarosalopes@gmail.com>
2020-02-13 21:45:48 -0300

    Ajuste no valor min e max de transferencia

--- src/controllers/TransactionsController.js
+++ src/controllers/TransactionsController.js
@@ -13,7 +13,7 @@ module.exports = {
         // Validação do body
         const validationBody = Joi.object().keys({
             numberAccount : Joi.string().length(8).required(),
-            amountTransferred : Joi.number().integer().greater(0).less(1000000),
+            amountTransferred : Joi.number().integer().greater(1000).less(10000000),
             cpf: Joi.required()
         })
 
